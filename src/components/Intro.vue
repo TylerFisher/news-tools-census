de49864631ae3a5d10c5f1363c2e76132cb0b4ae
Tyler Fisher <tyler@tylerjfisher.com>
2019-05-17 09:49:58 -0400

    update intro

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="intro">
     <h1>Newsroom tools census</h1>
-    <p>We’re a group funded by the Knight Foundation, the Lenfest Institute for Journalism and Facebook looking to understand what third-party software journalists use to perform their jobs and how well that software works.</p>
-    <p>Your feedback will help us begin our research. Any data we release from this survey will be anonymized and aggregated. We appreciate you taking this brief survey.</p>
+    <p>We’re a group funded by the Knight Foundation, the Lenfest Institute for Journalism and Facebook looking to understand what third-party software news professionals use to perform their jobs and how well that software works.</p>
+    <p>Your feedback will help us begin our research. We plan to share our research with the public, but any data we release from this survey will be anonymized and aggregated. We appreciate you taking this brief survey.</p>
   </div>
 </template>
 
