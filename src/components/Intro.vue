cbe0a9b7e5d35e133af028f255f8ab0ec040bb00
Tyler Fisher <tyler@tylerjfisher.com>
2019-09-20 12:05:30 -0400

    intro addition

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -3,6 +3,7 @@
     <h1>News tools census</h1>
     <p><a href="/">News Catalyst</a> is looking to understand what third-party software news professionals use to perform their jobs and how well that software works.</p>
     <p>Your feedback will help us begin our research. We plan to share our research with the public, but any data we release from this survey will be anonymized and aggregated. We appreciate you taking this brief survey.</p>
+    <p>With this knowledge, we will begin to identify the gaps in the industry for tooling and work to find solutions to those problems. We will also begin to identify tools we will offer through PressPass, our in-development single sign-on service for news tools.</p>
   </div>
 </template>
 
